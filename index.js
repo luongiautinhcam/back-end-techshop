db20ee3f5820a0a872902d50d454278762d7eb4f
Vinh <tranthanhvinh.info@gmail.com>
2023-04-28 16:45:22 +0700

    cors

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ const couponRouter = require("./routes/couponRoute");
 const enqRouter = require("./routes/enqRoute");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 dbConnect();
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
